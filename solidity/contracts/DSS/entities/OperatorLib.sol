bbfba3db0d5a0ef3bec7aef65e693d80576e96cc
Devinder <devinder@riskharbor.com>
2024-09-09 14:29:40 +0530

    [RIS-3278]: Add support for HyperlaneDSS in cli.

--- solidity/contracts/DSS/entities/OperatorLib.sol
+++ solidity/contracts/DSS/entities/OperatorLib.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.22;
 import {CheckpointsUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/CheckpointsUpgradeable.sol";
 import {Enrollment, EnrollmentStatus, EnumerableMapEnrollment} from "../../libs/EnumerableMapEnrollment.sol";
 import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
-import {SignatureCheckerUpgradeable} from
-    "@openzeppelin/contracts-upgradeable/utils/cryptography/SignatureCheckerUpgradeable.sol";
+import {SignatureCheckerUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/SignatureCheckerUpgradeable.sol";
 
 import {Constants} from "./Constants.sol";
 import {HyperlaneDSSLib} from "./HyperlaneDSSLib.sol";
@@ -33,7 +32,11 @@ library OperatorLib {
         bool isJailed;
     }
 
-    function registerOperator(HyperlaneDSSLib.Storage storage self, address operator, bytes memory data) internal {
+    function registerOperator(
+        HyperlaneDSSLib.Storage storage self,
+        address operator,
+        bytes memory data
+    ) internal {
         address signingKey = abi.decode(data, (address));
         State storage operatorState = self.operatorState[operator];
         operatorState.isRegistered = true;
@@ -43,7 +46,10 @@ library OperatorLib {
         updateOperatorSigningKey(self, operator, signingKey);
     }
 
-    function unregisterOperator(HyperlaneDSSLib.Storage storage self, address operator) internal {
+    function unregisterOperator(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal {
         State storage operatorState = self.operatorState[operator];
         if (!operatorState.isRegistered) revert OperatorNotRegistered();
         self.totalOperators--;
@@ -59,15 +65,21 @@ library OperatorLib {
     ) internal {
         State storage operatorState = self.operatorState[operator];
         for (uint256 i = 0; i < challengers.length; i++) {
-            (bool exists, Enrollment memory enrollment) =
-                operatorState.enrolledChallengers.tryGet(address(challengers[i]));
-            if (exists && enrollment.status == EnrollmentStatus.PENDING_UNENROLLMENT) {
+            (bool exists, Enrollment memory enrollment) = operatorState
+                .enrolledChallengers
+                .tryGet(address(challengers[i]));
+            if (
+                exists &&
+                enrollment.status == EnrollmentStatus.PENDING_UNENROLLMENT
+            ) {
                 revert PendingUnenrollment(challengers[i]);
             }
             if (enrollment.status == EnrollmentStatus.ENROLLED) break;
 
-            bool success =
-                operatorState.enrolledChallengers.set(address(challengers[i]), Enrollment(EnrollmentStatus.ENROLLED, 0));
+            bool success = operatorState.enrolledChallengers.set(
+                address(challengers[i]),
+                Enrollment(EnrollmentStatus.ENROLLED, 0)
+            );
             if (!success) revert UnableToEnrollIntoChallenger(challengers[i]);
             emit OperatorEnrolledToChallenger(operator, challengers[i]);
         }
@@ -80,31 +92,50 @@ library OperatorLib {
     ) internal {
         State storage operatorState = self.operatorState[operator];
         for (uint256 i = 0; i < challengers.length; i++) {
-            (bool exists, Enrollment memory enrollment) =
-                operatorState.enrolledChallengers.tryGet(address(challengers[i]));
+            (bool exists, Enrollment memory enrollment) = operatorState
+                .enrolledChallengers
+                .tryGet(address(challengers[i]));
             if (!exists || enrollment.status == EnrollmentStatus.UNENROLLED) {
                 revert OperatorNotEnrolledWithChallenger(challengers[i]);
             }
-            if (enrollment.status == EnrollmentStatus.PENDING_UNENROLLMENT) break;
+            if (enrollment.status == EnrollmentStatus.PENDING_UNENROLLMENT)
+                break;
 
             operatorState.enrolledChallengers.set(
-                address(challengers[i]), Enrollment(EnrollmentStatus.PENDING_UNENROLLMENT, uint248(block.number))
+                address(challengers[i]),
+                Enrollment(
+                    EnrollmentStatus.PENDING_UNENROLLMENT,
+                    uint248(block.number)
+                )
             );
             emit OperatorQueuedUnenrollmentFromChallenger(
-                operator, challengers[i], block.number, challengers[i].challengeDelayBlocks()
+                operator,
+                challengers[i],
+                block.number,
+                challengers[i].challengeDelayBlocks()
             );
         }
     }
 
-    function validateUnenrollment(State storage self, IRemoteChallenger challenger, address operator)
-        internal
-        returns (bool)
-    {
-        (bool exists, Enrollment memory enrollment) = self.enrolledChallengers.tryGet(address(challenger));
-        if (!exists || enrollment.status != EnrollmentStatus.PENDING_UNENROLLMENT) {
+    function validateUnenrollment(
+        State storage self,
+        IRemoteChallenger challenger,
+        address operator
+    ) internal returns (bool) {
+        (bool exists, Enrollment memory enrollment) = self
+            .enrolledChallengers
+            .tryGet(address(challenger));
+        if (
+            !exists ||
+            enrollment.status != EnrollmentStatus.PENDING_UNENROLLMENT
+        ) {
             revert ChallengerNotQueuedForUnenrollment(challenger);
         }
-        if (block.number < enrollment.unenrollmentStartBlock + challenger.challengeDelayBlocks()) {
+        if (
+            block.number <
+            enrollment.unenrollmentStartBlock +
+                challenger.challengeDelayBlocks()
+        ) {
             revert ChallengeDelayNotPassed(challenger);
         }
     }
@@ -117,20 +148,32 @@ library OperatorLib {
     ) internal {
         State storage operatorState = self.operatorState[operator];
         for (uint256 i = 0; i < challengers.length; i++) {
-            if (validate) validateUnenrollment(operatorState, IRemoteChallenger(challengers[i]), operator);
+            if (validate)
+                validateUnenrollment(
+                    operatorState,
+                    IRemoteChallenger(challengers[i]),
+                    operator
+                );
             operatorState.enrolledChallengers.remove(address(challengers[i]));
-            emit OperatorUnenrolledFromChallenger(operator, IRemoteChallenger(challengers[i]), block.number);
+            emit OperatorUnenrolledFromChallenger(
+                operator,
+                IRemoteChallenger(challengers[i]),
+                block.number
+            );
         }
     }
 
-    function jailOperator(HyperlaneDSSLib.Storage storage self, address operator) internal {
+    function jailOperator(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal {
         self.operatorState[operator].isJailed = true;
     }
 
-    function updateOperatorWeight(HyperlaneDSSLib.Storage storage self, address operator)
-        internal
-        returns (int256 delta)
-    {
+    function updateOperatorWeight(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal returns (int256 delta) {
         State storage operatorState = self.operatorState[operator];
         int256 delta;
         uint256 newWeight;
@@ -152,49 +195,70 @@ library OperatorLib {
         emit OperatorWeightUpdated(operator, oldWeight, newWeight);
     }
 
-    function updateOperatorSigningKey(HyperlaneDSSLib.Storage storage self, address operator, address newSigningKey)
-        internal
-    {
-        address oldSigningKey = address(uint160(self.operatorState[operator].operatorSigningKeyHistory.latest()));
+    function updateOperatorSigningKey(
+        HyperlaneDSSLib.Storage storage self,
+        address operator,
+        address newSigningKey
+    ) internal {
+        address oldSigningKey = address(
+            uint160(
+                self.operatorState[operator].operatorSigningKeyHistory.latest()
+            )
+        );
         if (newSigningKey == oldSigningKey) {
             return;
         }
-        self.operatorState[operator].operatorSigningKeyHistory.push(uint160(newSigningKey));
-        emit SigningKeyUpdate(operator, block.number, newSigningKey, oldSigningKey);
+        self.operatorState[operator].operatorSigningKeyHistory.push(
+            uint160(newSigningKey)
+        );
+        emit SigningKeyUpdate(
+            operator,
+            block.number,
+            newSigningKey,
+            oldSigningKey
+        );
     }
 
-    function isOperatorRegistered(HyperlaneDSSLib.Storage storage self, address operator) internal returns (bool) {
+    function isOperatorRegistered(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (bool) {
         return self.operatorState[operator].isRegistered;
     }
 
-    function getOperatorChallengers(HyperlaneDSSLib.Storage storage self, address operator)
-        internal
-        view
-        returns (address[] memory)
-    {
+    function getOperatorChallengers(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (address[] memory) {
         return self.operatorState[operator].enrolledChallengers.keys();
     }
 
-    function getEnrollmentStatus(HyperlaneDSSLib.Storage storage self, address operator, address challenger)
-        internal
-        view
-        returns (Enrollment memory enrollment)
-    {
-        (, enrollment) = self.operatorState[operator].enrolledChallengers.tryGet(challenger);
+    function getEnrollmentStatus(
+        HyperlaneDSSLib.Storage storage self,
+        address operator,
+        address challenger
+    ) internal view returns (Enrollment memory enrollment) {
+        (, enrollment) = self
+            .operatorState[operator]
+            .enrolledChallengers
+            .tryGet(challenger);
     }
 
-    function getOperatorWeight(HyperlaneDSSLib.Storage storage self, address operator)
-        internal
-        view
-        returns (uint256)
-    {
+    function getOperatorWeight(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (uint256) {
         uint256 weight;
         // Includes vaults queued for exit too. Need to removed vaults queued for unstaking
-        address[] memory vaults = self.core.fetchVaultsStakedInDSS(operator, address(this)); // this lib should be internal else this will fail
+        address[] memory vaults = self.core.fetchVaultsStakedInDSS(
+            operator,
+            address(this)
+        ); // this lib should be internal else this will fail
         for (uint256 i; i < vaults.length; i++) {
             //instead of `totalAssets()` need to fetch assets not queued for withdrawals
             weight +=
-                IKarakBaseVault(vaults[i]).totalAssets() * getAssetWeight(self, IKarakBaseVault(vaults[i]).asset());
+                IKarakBaseVault(vaults[i]).totalAssets() *
+                getAssetWeight(self, IKarakBaseVault(vaults[i]).asset());
         }
         weight = weight / Constants.BPS;
 
@@ -205,44 +269,60 @@ library OperatorLib {
         }
     }
 
-    function getAssetWeight(HyperlaneDSSLib.Storage storage self, address asset)
-        internal
-        view
-        returns (uint256 weight)
-    {
+    function getAssetWeight(
+        HyperlaneDSSLib.Storage storage self,
+        address asset
+    ) internal view returns (uint256 weight) {
         (, weight) = self.assetToWeightMap[self.quorumIndex].tryGet(asset);
     }
 
-    function getOperatorSigningKeyAtBlock(HyperlaneDSSLib.Storage storage self, address operator, uint256 blockNumber)
-        internal
-        view
-        returns (address)
-    {
-        return address(uint160(self.operatorState[operator].operatorSigningKeyHistory.getAtBlock(blockNumber)));
+    function getOperatorSigningKeyAtBlock(
+        HyperlaneDSSLib.Storage storage self,
+        address operator,
+        uint256 blockNumber
+    ) internal view returns (address) {
+        return
+            address(
+                uint160(
+                    self
+                        .operatorState[operator]
+                        .operatorSigningKeyHistory
+                        .getAtBlock(blockNumber)
+                )
+            );
     }
 
-    function getLastestOperatorSigningKey(HyperlaneDSSLib.Storage storage self, address operator)
-        internal
-        view
-        returns (address)
-    {
-        return address(uint160(self.operatorState[operator].operatorSigningKeyHistory.latest()));
+    function getLastestOperatorSigningKey(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (address) {
+        return
+            address(
+                uint160(
+                    self
+                        .operatorState[operator]
+                        .operatorSigningKeyHistory
+                        .latest()
+                )
+            );
     }
 
-    function getLastCheckpointOperatorWeight(HyperlaneDSSLib.Storage storage self, address operator)
-        internal
-        view
-        returns (uint256)
-    {
+    function getLastCheckpointOperatorWeight(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (uint256) {
         return self.operatorState[operator].operatorWeightHistory.latest();
     }
 
-    function getOperatorWeightAtBlock(HyperlaneDSSLib.Storage storage self, address operator, uint32 blockNumber)
-        internal
-        view
-        returns (uint256)
-    {
-        return self.operatorState[operator].operatorWeightHistory.getAtBlock(blockNumber);
+    function getOperatorWeightAtBlock(
+        HyperlaneDSSLib.Storage storage self,
+        address operator,
+        uint32 blockNumber
+    ) internal view returns (uint256) {
+        return
+            self.operatorState[operator].operatorWeightHistory.getAtBlock(
+                blockNumber
+            );
     }
 
     function isValidSignature(
@@ -252,16 +332,23 @@ library OperatorLib {
         bytes memory signature,
         uint32 blockNumber
     ) internal view {
-        address signer = getOperatorSigningKeyAtBlock(self, operator, blockNumber);
-        if (!signer.isValidSignatureNow(dataHash, signature)) revert InvalidSignature();
+        address signer = getOperatorSigningKeyAtBlock(
+            self,
+            operator,
+            blockNumber
+        );
+        if (!signer.isValidSignatureNow(dataHash, signature))
+            revert InvalidSignature();
     }
 
-    function getOperatorRestakedVaults(HyperlaneDSSLib.Storage storage self, address operator)
-        internal
-        view
-        returns (address[] memory restakedVaults)
-    {
-        address[] memory vaults = self.core.fetchVaultsStakedInDSS(operator, address(this));
+    function getOperatorRestakedVaults(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (address[] memory restakedVaults) {
+        address[] memory vaults = self.core.fetchVaultsStakedInDSS(
+            operator,
+            address(this)
+        );
 
         uint256 vaultCountWithAssetsInQuorum = 0;
         for (uint256 i = 0; i < vaults.length; i++) {
@@ -283,7 +370,10 @@ library OperatorLib {
         }
     }
 
-    function isJailed(HyperlaneDSSLib.Storage storage self, address operator) internal view returns (bool) {
+    function isJailed(
+        HyperlaneDSSLib.Storage storage self,
+        address operator
+    ) internal view returns (bool) {
         return self.operatorState[operator].isJailed;
     }
 }
--- solidity/contracts/interfaces/DSS/vendored/ICore.sol
+++ solidity/contracts/interfaces/DSS/vendored/ICore.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.22;
 
+import {IDSS} from "./IDSS.sol";
+
 interface ICore {
     function registerDSS(uint256 maxSlashablePercentageWad) external;
+    function registerOperatorToDSS(
+        IDSS dss,
+        bytes memory registrationHookData
+    ) external;
+    function unregisterOperatorFromDSS(IDSS dss) external;
 
     /* ============ VIEW FUNCTIONS ============ */
     function getOperatorVaults(
@@ -15,5 +22,9 @@ interface ICore {
     function extSloads(
         bytes32[] calldata slots
     ) external view returns (bytes32[] memory res);
+    function isOperatorRegisteredToDSS(
+        address operator,
+        IDSS dss
+    ) external view returns (bool);
     /* ======================================== */
 }
